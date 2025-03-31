d8a9ca214f898e3f787354fe6aa34afeb7b55c83
hipeoplea <cheremisova1968.ru@gmail.com>
2025-03-31 11:21:51 +0300

    0-49

---
+++ commit26/E.java
@@ -0,0 +1,30 @@
+public class E implements K, D {
+
+    private double i = 100.500;
+
+    private byte j = 1;
+
+    public String kk() {
+        return "Yes";
+    }
+
+    public int af() {
+        return -1;
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public void aa() {
+        System.out.println("Hello world!");
+    }
+
+    public double ee() {
+        return java.lang.Math.PI;
+    }
+
+    public long ac() {
+        return 111;
+    }
+}

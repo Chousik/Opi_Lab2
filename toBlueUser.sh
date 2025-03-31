2ad1dbabacd7ae7619c3ae0892a6c90a4a1b1e06
chousik <Zaxap2018@yandex.ru>
2025-03-31 13:11:45 +0300

    add blue&red users

---
+++ toBlueUser.sh
@@ -0,0 +1,3 @@
+#f*ck. don't touch it in main git dir
+git config --local user.name "chousik"
+git config --local user.mail "Zaxap2018@yandex.ru"
---
+++ toRedUser.sh
@@ -0,0 +1,3 @@
+#f*ck. don't touch it in main git dir
+git config --local user.name "hipeoplea"
+git config --local user.mail "red@zahar.ru"
